2e433e8ece2e3a6ca34937cb8653914aa96311a1
Rafael Oliveira <110493885+rafaeloliveira20@users.noreply.github.com>
2022-09-23 16:47:03 -0300

    .

---
+++ estrutura condicional/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int nota1 = 5;
+        int nota2 = 1;
+        int media = (nota1+nota2)/2;
+        // se nota maior ou igual a 70 o aluno está aprovado
+        // usando if - else
+        if(media >= 7){
+            System.out.println("aprovado");
+        }else {
+            System.out.println("não aprovado");
+        }
+
+
+
+
+    }
+}
